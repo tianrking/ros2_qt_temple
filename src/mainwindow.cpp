99a6a59e241708285e36ad3812c89d14a1d74b33
w0x7ce <me@w0x7ce.eu>
2023-03-11 21:33:43 +0800

    humble

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     QImage img;
-    img.load(":/icon/images/foxy.jpg");
+    img.load(":/icon/images/humble.jpg");
     img.scaled(ui->label->width(),ui->label->height());
     ui->label->setPixmap(QPixmap::fromImage(img).scaled(ui->label->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
     commNode=new rclcomm();
